309c50d5834bef4b7da1a26052cb57c5c76d70f8
kido44 <t.kidokido@ezweb.ne.jp>
2019-11-18 13:53:16 +0900

    修正

--- Gemfile
+++ Gemfile
@@ -24,7 +24,7 @@ gem 'coffee-rails', '~> 4.2'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
-gem 'turbolinks', '~> 5'
+# gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
@@ -38,6 +38,11 @@ gem 'jbuilder', '~> 2.5'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
+  gem 'capistrano'
+  gem 'capistrano-rbenv'
+  gem 'capistrano-bundler'
+  gem 'capistrano-rails'
+  gem 'capistrano3-unicorn'
 end
 
 group :development do
@@ -51,8 +56,15 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
-gem "haml-rails"
+gem 'haml-rails'
+gem 'erb2haml'
 gem "font-awesome-rails"
 gem 'devise'
 gem 'carrierwave'
+gem 'fog-aws'
 gem 'mini_magick'
+gem 'pry-rails'
+
+group :production do
+  gem 'unicorn', '5.4.1'
+end
--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,99 +1,99 @@
 $(function(){
-    // HTML作成
-    function buildHTML(comment){
-      var image_url = (comment.image)? `<image class="lower-message_image" src="${comment.image}">`:"";
-      var html = `<li class="timeline__bodyList" data-id="${comment.id}">
-                    <div class="timeline__bodyInfoWrap">
-                    <div class="timeline__bodyName">
-                    ${comment.user_name}
-                    </div>
-                    <div class="timeline__bodyDate">
-                    ${comment.created_at}
-                    </div>
-                    </div>
-                    <div class="timeline__bodyMessage">
-                    ${comment.content}
-                    </div>
-                    ${image_url}
-                  </li>`
-      return html;
-    }
-  
-    //メッセージ送信の非同期化
-    $('#new_message').on('submit', function(e){
-      e.preventDefault();
-      var formData = new FormData(this);
-      var url = $(this).attr('action');
-      $('.form__submit').removeAttr('data-disable-with');
+  // HTML作成
+  function buildHTML(comment){
+    var image_url = (comment.image)? `<image class="lower-message_image" src="${comment.image}">`:"";
+    var html = `<li class="timeline__bodyList" data-id="${comment.id}">
+                  <div class="timeline__bodyInfoWrap">
+                  <div class="timeline__bodyName">
+                  ${comment.user_name}
+                  </div>
+                  <div class="timeline__bodyDate">
+                  ${comment.created_at}
+                  </div>
+                  </div>
+                  <div class="timeline__bodyMessage">
+                  ${comment.content}
+                  </div>
+                  ${image_url}
+                </li>`
+    return html;
+  }
+
+  //メッセージ送信の非同期化
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $('.form__submit').removeAttr('data-disable-with');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('#message').append(html);
+      $('#new_message')[0].reset();
+      $('.timeline__body').animate({scrollTop: $(".timeline__body")[0].scrollHeight }, 'fast');
+    })
+    .fail(function() {
+      alert('メッセージを送信できません');
+    });
+  })
+
+  //自動更新
+  // function reloadMessages(){
+  //   var last_message_id = $('.timeline__bodyList').last().data('id');
+  //   var href = 'api/messages'
+  //   $.ajax({
+  //   url: href,
+  //   type: 'GET',
+  //   data:{id: last_message_id},
+  //   dataType: 'json'
+  //   })
+
+  //   .done(function(messages){
+  //     console.log(messages);
+  //     messages.forEach(function(message){
+  //       var insertHTML = buildHTML(message)
+  //       $('#message').append(insertHTML)
+  //     });
+
+  //     $('.timeline__body').animate({scrollTop: $('.timeline__body')[0].scrollHeight}, 'fast');
+  //   })
+
+  //   .fail(function(){
+  //     console.log('error');
+  //   });
+  // };
+  // setInterval(reloadMessages, 5000);
+
+  var interval = setInterval(function() {
+    if (location.href.match(/\/groups\/\d+\/messages/)){
+      $('.timeline__body').animate({scrollTop: $('.timeline__body')[0].scrollHeight}, 'fast');
+      var last_message_id = $('.timeline__bodyList').last().data('id');
+      var href = 'api/messages'
       $.ajax({
-        url: url,
-        type: "POST",
-        data: formData,
-        dataType: 'json',
-        processData: false,
-        contentType: false
+        url: href,
+        type: "GET",
+        data: {id: last_message_id},
+        dataType: "json"
       })
-      .done(function(data){
-        var html = buildHTML(data);
-        $('#message').append(html);
-        $('#new_message')[0].reset();
-        $('.timeline__body').animate({scrollTop: $(".timeline__body")[0].scrollHeight }, 'fast');
+      .done(function(messages) {
+        messages.forEach(function(message) {
+          var insertHTML = buildHTML(message)
+          $('#message').append(insertHTML)
+          $('.timeline__body').animate({scrollTop: $('.timeline__body')[0].scrollHeight}, 'fast');
+        })
       })
       .fail(function() {
-        alert('メッセージを送信できません');
+        alert('自動更新に失敗しました');
       });
-    })
-  
-    //自動更新
-    // function reloadMessages(){
-    //   var last_message_id = $('.timeline__bodyList').last().data('id');
-    //   var href = 'api/messages'
-    //   $.ajax({
-    //   url: href,
-    //   type: 'GET',
-    //   data:{id: last_message_id},
-    //   dataType: 'json'
-    //   })
-  
-    //   .done(function(messages){
-    //     console.log(messages);
-    //     messages.forEach(function(message){
-    //       var insertHTML = buildHTML(message)
-    //       $('#message').append(insertHTML)
-    //     });
-  
-    //     $('.timeline__body').animate({scrollTop: $('.timeline__body')[0].scrollHeight}, 'fast');
-    //   })
-  
-    //   .fail(function(){
-    //     console.log('error');
-    //   });
-    // };
-    // setInterval(reloadMessages, 5000);
-  
-    var interval = setInterval(function() {
-      if (location.href.match(/\/groups\/\d+\/messages/)){
-        $('.timeline__body').animate({scrollTop: $('.timeline__body')[0].scrollHeight}, 'fast');
-        var last_message_id = $('.timeline__bodyList').last().data('id');
-        var href = 'api/messages'
-        $.ajax({
-          url: href,
-          type: "GET",
-          data: {id: last_message_id},
-          dataType: "json"
-        })
-        .done(function(messages) {
-          messages.forEach(function(message) {
-            var insertHTML = buildHTML(message)
-            $('#message').append(insertHTML)
-            $('.timeline__body').animate({scrollTop: $('.timeline__body')[0].scrollHeight}, 'fast');
-          })
-        })
-        .fail(function() {
-          alert('自動更新に失敗しました');
-        });
-      } else {
-          clearInterval(interval);
-        }
-    } , 5000 );
-  })
+    } else {
+        clearInterval(interval);
+      }
+  } , 5000 );
+})
